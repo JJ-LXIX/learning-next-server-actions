90fcf76acb9b422efc4879f7db9730e511841458
Jagrath Shetty <jagrathshettylxix@gmail.com>
2024-02-21 16:57:43 +0530

    added Todo component to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import { db } from "@/db";
 import { todos } from "@/db/schema";
 import TodoForm from "./components/TodoForm/TodoForm";
+import Todo from "./components/Todo/Todo";
 
 export default async function Home() {
   const result = await db.select().from(todos);
   return (
     <main className="min-h-screen p-10">
       <TodoForm />
+      <div className="w-full grid gap-4 grid-cols-3 mt-7">
+        {result?.map((todo) => {
+          return <Todo key={todo.id} todo={todo} />;
+        })}
+      </div>
     </main>
   );
 }
