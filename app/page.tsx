7738b416eb123fe4c6befabe50430ae2137ec1ba
Jagrath Shetty <jagrathshettylxix@gmail.com>
2024-02-21 16:57:30 +0530

    added server action

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-import Image from "next/image";
+import { db } from "@/db";
+import { todos } from "@/db/schema";
+import TodoForm from "./components/TodoForm/TodoForm";
 
-export default function Home() {
+export default async function Home() {
+  const result = await db.select().from(todos);
   return (
+    <main className="min-h-screen p-10">
       <TodoForm />
     </main>
   );
