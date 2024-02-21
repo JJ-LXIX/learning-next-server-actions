987688a31ba94f64ba097207480c263acf941780
Jagrath Shetty <jagrathshettylxix@gmail.com>
2024-02-21 16:55:13 +0530

    added drizzle connection to supabase

---
+++ db/index.ts
@@ -0,0 +1,7 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as schema from "./schema";
+
+const connectionString = process.env.DATABASE_URL;
+export const client = postgres(connectionString as string);
+export const db = drizzle(client, { schema });
