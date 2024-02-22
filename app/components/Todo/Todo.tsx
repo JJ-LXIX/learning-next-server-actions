5a0b283b63794fff9bb21bbb627b0d501d8adc69
Jagrath Shetty <jagrathshettylxix@gmail.com>
2024-02-22 14:32:04 +0530

    added setOptimisticTodo to delete post

--- app/components/Todo/Todo.tsx
+++ app/components/Todo/Todo.tsx
@@ -1,23 +1,37 @@
 "use client";
 import { removeTodo } from "@/app/actions/actions";
-import React from "react";
+import React, { startTransition } from "react";
 
 type Props = {
   todo: {
     id?: number;
     todo: string | null;
   };
+  setOptimisticTodo: any;
 };
 
-export default function Todo({ todo }: Props) {
+export default function Todo({ todo, setOptimisticTodo }: Props) {
+  function buttonClick(id: number) {
+    startTransition(() => {
+      {
+        setOptimisticTodo({
+          action: "delete",
+          newTodo: { id },
+        });
+      }
+    });
+
+    removeTodo(id);
+  }
+
   return (
     <div
       className="min-h-52 bg-black text-white w-full flex p-4 rounded-lg relative"
-      key={todo.id}
+      key={todo?.id}
     >
-      {todo.todo}
+      {todo?.todo}
       <button
-        onClick={() => removeTodo(todo.id!)}
+        onClick={() => buttonClick(todo.id!)}
         className="bg-red-800 w-10 h-10 absolute right-3 bottom-3 rounded-md"
       >
         X
