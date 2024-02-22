6b70aef44a42190f207f2087c6592b1d46adbdf4
Jagrath Shetty <jagrathshettylxix@gmail.com>
2024-02-22 14:31:44 +0530

    added setOptimisticTodo as prop

--- app/components/TodoList/TodoList.tsx
+++ app/components/TodoList/TodoList.tsx
@@ -2,18 +2,36 @@
 import React from "react";
 import Todo from "../Todo/Todo";
 
+type Todo = {
+  id?: number;
+  todo: string | null;
+};
+
 type Props = {
   optimisticTodos: {
     id?: number;
     todo: string | null;
   }[];
+  setOptimisticTodo: (action: {
+    action?: string | undefined;
+    newTodo: Todo;
+  }) => void;
 };
 
-export default function TodoList({ optimisticTodos }: Props) {
+export default function TodoList({
+  optimisticTodos,
+  setOptimisticTodo,
+}: Props) {
   return (
     <div className="w-full grid gap-4 grid-cols-3 mt-7">
       {optimisticTodos?.map((todo) => {
-        return <Todo key={todo.id} todo={todo} />;
+        return (
+          <Todo
+            key={todo?.id}
+            todo={todo}
+            setOptimisticTodo={setOptimisticTodo}
+          />
+        );
       })}
     </div>
   );
